3ada090e2a085352cf052b3d442b20c37a4370d3
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-04-04 12:21:02 +0200

    1. feladatrész kész

---
+++ etelClass/Etel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etelClass
+{
+	internal abstract class Etel
+	{
+		double caloriesPerDkg;
+		double weight;
+		string name;
+		bool IsItDairyFree;
+
+		public double CaloriesPerDkg { get => caloriesPerDkg; set => caloriesPerDkg = value; }
+		public double Weight { get => weight; set => weight = value; }
+		public string Name { get => name; set => name = value; }
+
+		protected Etel(double caloriesPerDkg, double weight, string name, bool IsItDairyFree = false, bool isItDairyFree = false)
+		{
+			this.caloriesPerDkg = caloriesPerDkg;
+			this.weight = weight;
+			this.name = name;
+		}
+
+		protected Etel(double caloriesPerDkg, double weight, string name)
+		{
+			this.caloriesPerDkg = caloriesPerDkg;
+			this.weight = weight;
+			this.name = name;
+			IsItDairyFree = false;
+		}
+
+		public void IsDairyFree()
+		{
+			IsItDairyFree = true; 
+		}
+		public abstract double CountCalories();
+	}
+}
